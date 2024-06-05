58a8976b55aba4e675f48eab1c792c29b91666c3
Varad J <varad.gcs@gmail.com>
2024-06-05 15:39:19 +0530

    interviewStack: Set up media and static url

--- interviewStack/interviewStack/settings.py
+++ interviewStack/interviewStack/settings.py
@@ -132,11 +132,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Media files
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
