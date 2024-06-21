208c42a622c7df28f1b4e6aaf8fff55831399378
Varad <varad.gcs@gmail.com>
2024-06-21 21:31:34 +0530

    Rebrand to commongrounds

--- commongrounds/commongrounds/asgi.py
+++ commongrounds/commongrounds/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for interviewStack project.
+ASGI config for commongrounds project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interviewStack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commongrounds.settings')
 
 application = get_asgi_application()
--- commongrounds/commongrounds/wsgi.py
+++ commongrounds/commongrounds/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for interviewStack project.
+WSGI config for commongrounds project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interviewStack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commongrounds.settings')
 
 application = get_wsgi_application()
