9e42818042d8d657b4f7a5ceead49df416115ebc
Varad J <varad.gcs@gmail.com>
2024-09-18 19:40:10 +0530

    Remove unused urls

--- smartdeskai/core/urls.py
+++ smartdeskai/core/urls.py
@@ -3,7 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('people/', people, name='people'),
-    path('schedule/', schedule, name='schedule'),
     path('add_user/', user_creation, name="onboarding")
 ]
