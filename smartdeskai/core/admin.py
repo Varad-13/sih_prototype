e7c81d692684d5eb5e5824ba02a16634615d3eb1
Varad J <varad.gcs@gmail.com>
2024-09-18 19:35:13 +0530

    Remove unused models from admin

--- smartdeskai/core/admin.py
+++ smartdeskai/core/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from .models import Image, Userprofile, Timings, Locality, ServiceTypes, Service, Venue, Schedule, Address
+from .models import Image, Userprofile
 
 admin.site.register(Image)
 admin.site.register(Userprofile)
-admin.site.register(Timings)
-admin.site.register(Locality)
-admin.site.register(Address)
-admin.site.register(ServiceTypes)
-admin.site.register(Service)
-admin.site.register(Venue)
-admin.site.register(Schedule)
+
