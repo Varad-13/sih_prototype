3ea3e016fbe444519f28e38f050f6a115ceb46c7
Varad J <varad.gcs@gmail.com>
2024-09-18 10:57:16 +0530

    Refactor commongrounds to smartdeskai

--- smartdeskai/smartdeskai/asgi.py
+++ smartdeskai/smartdeskai/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for commongrounds project.
+ASGI config for smartdeskai project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commongrounds.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartdeskai.settings')
 
 application = get_asgi_application()
--- smartdeskai/smartdeskai/wsgi.py
+++ smartdeskai/smartdeskai/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for commongrounds project.
+WSGI config for smartdeskai project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commongrounds.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartdeskai.settings')
 
 application = get_wsgi_application()
